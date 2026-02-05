f247abe56b630d938a223e713a380c3ebe7b8499
opilane <opilane@HP-CZC2349HTS>
2026-02-05 12:24:04 +0200

    Add project files.

---
+++ NaidisRepo/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaidisRepo
+{
+    internal class Naidis_funktsioonid
+    {
+        internal static float Summa(float a, float b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ NaidisRepo/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+using System.Text;
+
+namespace NaidisRepo
+
+{
+    public class StartPage
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("1. Osa Andmetüübid");
+
+            string tekst = "Tere tulemast C# maailma!";
+            Console.WriteLine($"Oli loodud muutuja tekst, mis võrdub: {tekst}");
+            Console.WriteLine("Oli loodud muutuja tekst, mis võrdub: {0}", tekst);
+
+            Console.Write("Mis on sinu nimi?: ");
+            string nimi = Console.ReadLine();
+            Console.Write("Kui vana sa oled?");
+
+            try
+            {
+                int vanus = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Tere {nimi}. Sa oled {vanus} aastat vana");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            try
+            {
+                Console.Write("Arv 1");
+                float a = float.Parse(Console.ReadLine());
+                Console.Write("Arv 2");
+                float b = float.Parse(Console.ReadLine());
+                float vastus = Naidis_funktsioonid.Summa(a, b);
+                Console.WriteLine($"Summa {a} ja {b} võrdub {vastus}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
