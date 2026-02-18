3d00be541c97c62074a9bc12c518ee406ad5cdde
Artyom <234121450+TheIndomitableHumanSpiritTTHK@users.noreply.github.com>
2026-02-18 19:47:33 +0200

    Refactored Program.cs, finished ÜL2

--- NaidisRepo/Program.cs
+++ NaidisRepo/Program.cs
@@ -1,113 +1,211 @@
-﻿
-
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+﻿using System.Text;
 
 namespace NaidisRepo
 
 {
     public class StartPage
     {
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("3. Osa. Kordused ja Listid, massivid");
-            for (int i = 0; i < 10; i++)
+            Console.OutputEncoding = Encoding.UTF8;
+
+            while (true)
             {
-                Console.WriteLine($"Tere, {i + 1}");
+                Console.Clear();
+
+                Console.WriteLine("Tere tulemast minu programmisse!");
+                Console.WriteLine("Milline ülesanne osa kas te tahaksite jooksma panna?");
+
+                Console.WriteLine("1. Osa Andmetüübid");
+                Console.WriteLine("2. Osa Funktsioonid");
+                Console.WriteLine("3. Osa");
+                Console.WriteLine("4. Osa");
+                Console.WriteLine("5. Osa");
+                Console.WriteLine("0. Välja");
+
+                Console.Write("Sinu valik: ");
+                string valik = Console.ReadLine();
+
+                switch (valik)
+                {
+                    case "1":
+                        //Kood 1. osale
+                        break;
+                    case "2":
+                        Teine_osa();
+                        break;
+                    case "3":
+                        //Kood 3. osale
+                        break;
+                    case "4":
+                        //Kood 4. osale
+                        break;
+                    case "5":
+                        //Kood 5. osale
+                        break;
+                    case "0":
+                        Console.Clear();
+                        Console.WriteLine("Äitah kasutamise eest! Tulge tagasi mõni kord veel.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Tundmatu valik. Vajuta Enter...");
+                        Console.ReadLine();
+                        continue;
+                }
             }
-            Console.WriteLine("While: ");
-            int j = 0;
-            while (j < 10)
-            {
-                Console.WriteLine($"Tere, {j + 1}");
-                j++;
-            }
-            Console.WriteLine("Do while: ");
-            do
-            {
-                Console.WriteLine($"Tere, {j}");
-                j--;
-            }
-            while (j != 0);
-
-            List<Isik> inimesed = new List<Isik>();
-
-            for (int i = 0; i < 3; i++)
-            {
-                Console.Write("Sisesta nimi: ");
-                string nimi = Console.ReadLine();
-
-                Console.Write("Sisesta vanus: ");
-                int vanus = int.Parse(Console.ReadLine());
-
-                Isik uusIsik = new Isik();
-                uusIsik.Nimi = nimi;
-                uusIsik.Vanus = vanus;
-
-                inimesed.Add(uusIsik);
-            }
-
-            Console.WriteLine("\nKõik inimesed tervitavad:\n");
-
-            foreach (Isik isik in inimesed)
-            {
-                isik.Tervita();
-            }
-
-
-
-            //int[] arvud = new int[5];
-
-            //arvud = Naidis_funktsioonid.Täida_massiiv(arvud);
-            //Naidis_funktsioonid.Massiivide_kuvamine(arvud);
-            //Naidis_funktsioonid.Muuda_element_massiivis(arvud);
-
-
-            //Console.OutputEncoding = Encoding.UTF8;
-            //Console.WriteLine("1. Osa Andmetüübid");
-
-            //string tekst = "Tere tulemast C# maailma!";
-            //Console.WriteLine($"Oli loodud muutuja tekst, mis võrdub: {tekst}");
-            //Console.WriteLine("Oli loodud muutuja tekst, mis võrdub: {0}", tekst);
-
-            //Console.Write("Mis on sinu nimi?: ");
-            //string nimi = Console.ReadLine();
-            //Console.Write("Kui vana sa oled?");
-
-            //try
-            //{
-            //    int vanus = int.Parse(Console.ReadLine());
-            //    Console.WriteLine($"Tere {nimi}. Sa oled {vanus} aastat vana");
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e);
-            //    throw;
-            //}
-
-            //try
-            //{
-            //    Console.Write("Arv 1");
-            //    float a = float.Parse(Console.ReadLine());
-            //    Console.Write("Arv 2");
-            //    float b = float.Parse(Console.ReadLine());
-            //    float vastus = Naidis_funktsioonid.Summa(a, b);
-            //    Console.WriteLine($"Summa {a} ja {b} võrdub {vastus}");
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e);
-            //}
-
-            ////Random rnd = new Random();
-            ////int juhuslik_arv = rnd.Next(-5, 25);
-            ////Console.WriteLine(Naidis_funktsioonid.Kuu_nimetus(juhuslik_arv));
-            ////juhuslik_arv = rnd.Next(-5, 25);
-            ////tekst = Naidis_funktsioonid.Kuu_nimetus(juhuslik_arv)
-
-            //Teine_class.JukuKino();
-
         }
 
+
+        public static void Teine_osa()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("2. osa Funktsioonid");
+                Console.WriteLine("Milline ÜL kas te tahaksite üle vaadata?");
+                Console.WriteLine();
+
+                Console.WriteLine("1  - ÜL: Juku kino pilet");
+                Console.WriteLine("2  - ÜL: Kaks pinginaabrit");
+                Console.WriteLine("3  - ÜL: Ristküliku toa pindala + remondi hind");
+                Console.WriteLine("4  - ÜL: Leia alghind (30% soodustuse alghinnaga)");
+                Console.WriteLine("5  - ÜL: Temperatuur > 18?");
+                Console.WriteLine("6  - ÜL: Pikkuse kategooria - lühike / keskmine / pikk");
+                Console.WriteLine("7  - ÜL: Pikkus + sugu - lühike / keskmine / pikk");
+                Console.WriteLine("8  - ÜL: Poes ostud");
+                Console.WriteLine("0  - Tagasi");
+                Console.WriteLine();
+
+                Console.Write("Sinu valik: ");
+                string valik = Console.ReadLine();
+
+                switch (valik)
+                {
+                    case "1":
+                        Teine_osa_funktsioonid.JukuKino();
+                        break;
+                    case "2":
+                        Teine_osa_funktsioonid.Pinginaabrid();
+                        break;
+                    case "3":
+                        Teine_osa_funktsioonid.TubaPindalaJaRemont();
+                        break;
+                    case "4":
+                        Teine_osa_funktsioonid.Alghind30Soodustus();
+                        break;
+                    case "5":
+                        Teine_osa_funktsioonid.Temperatuur();
+                        break;
+                    case "6":
+                        Teine_osa_funktsioonid.PikkusKategooria();
+                        break;
+                    case "7":
+                        Teine_osa_funktsioonid.PikkusJaSugu();
+                        break;
+                    case "8":
+                        Teine_osa_funktsioonid.PoodOstud();
+                        break;
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Tundmatu valik. Vajuta Enter...");
+                        Console.ReadLine();
+                        continue;
+                }
+                Console.WriteLine("\nVajuta Enter, et menüüsse tagasi minna...");
+                Console.ReadLine();
+            }
+        }
+
+
+        //Console.WriteLine("1. Osa Andmetüübid");
+
+        //string tekst = "Tere tulemast C# maailma!";
+        //Console.WriteLine($"Oli loodud muutuja tekst, mis võrdub: {tekst}");
+        //Console.WriteLine("Oli loodud muutuja tekst, mis võrdub: {0}", tekst);
+
+        //Console.Write("Mis on sinu nimi?: ");
+        //string nimi = Console.ReadLine();
+        //Console.Write("Kui vana sa oled?");
+
+        //try
+        //{
+        //    int vanus = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"Tere {nimi}. Sa oled {vanus} aastat vana");
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine(e);
+        //    throw;
+        //}
+
+        //try
+        //{
+        //    Console.Write("Arv 1");
+        //    float a = float.Parse(Console.ReadLine());
+        //    Console.Write("Arv 2");
+        //    float b = float.Parse(Console.ReadLine());
+        //    float vastus = Naidis_funktsioonid.Summa(a, b);
+        //    Console.WriteLine($"Summa {a} ja {b} võrdub {vastus}");
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine(e);
+        //}
+
+        ////Random rnd = new Random();
+        ////int juhuslik_arv = rnd.Next(-5, 25);
+        ////Console.WriteLine(Naidis_funktsioonid.Kuu_nimetus(juhuslik_arv));
+        ////juhuslik_arv = rnd.Next(-5, 25);
+        ////tekst = Naidis_funktsioonid.Kuu_nimetus(juhuslik_arv)
+
+        //Teine_class.JukuKino();
+
+        // Console.WriteLine("3. Osa. Kordused ja Listid, massivid");
+        //            for (int i = 0; i< 10; i++)
+        //            {
+        //                Console.WriteLine($"Tere, {i + 1}");
+        //            }
+        //    Console.WriteLine("While: ");
+        //            int j = 0;
+        //            while (j< 10)
+        //            {
+        //                Console.WriteLine($"Tere, {j + 1}");
+        //                j++;
+        //            }
+        //Console.WriteLine("Do while: ");
+        //do
+        //{
+        //    Console.WriteLine($"Tere, {j}");
+        //    j--;
+        //}
+        //while (j != 0);
+
+        //List<Isik> inimesed = new List<Isik>();
+
+        //for (int i = 0; i < 3; i++)
+        //{
+        //    Console.Write("Sisesta nimi: ");
+        //    string nimi = Console.ReadLine();
+
+        //    Console.Write("Sisesta vanus: ");
+        //    int vanus = int.Parse(Console.ReadLine());
+
+        //    Isik uusIsik = new Isik();
+        //    uusIsik.Nimi = nimi;
+        //    uusIsik.Vanus = vanus;
+
+        //    inimesed.Add(uusIsik);
+        //}
+
+        //Console.WriteLine("\nKõik inimesed tervitavad:\n");
+
+        //foreach (Isik isik in inimesed)
+        //{
+        //    isik.Tervita();
+        //}
     }
-    }
+}
---
+++ NaidisRepo/Teine_osa_funktsioonid.cs
@@ -0,0 +1,357 @@
+﻿namespace NaidisRepo
+{
+    public class Teine_osa_funktsioonid
+    {
+        public static void JukuKino()
+        {
+            Console.Write("Sisesta eesnimi: ");
+            string eesnimi = Console.ReadLine();
+            int vanus = 0;
+
+            if (eesnimi.ToLower() != "juku")
+            {
+                Console.WriteLine("See ülesanne on Juku jaoks — sinu nimi pole Juku.");
+                return;
+            }
+
+            Console.WriteLine("Lähme Jukuga kinno!");
+            while (true)
+            {
+                Console.Write("Sisesta Juku vanus: ");
+                try
+                {
+                    vanus = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Palun sisesta korrektne täisarv.");
+                }
+            }
+
+            if (vanus < 0 || vanus > 100)
+            {
+                Console.WriteLine("Vigased andmed. Vanus peab olema suurem kui 0 ja väiksem kui 100.");
+                return;
+            }
+
+            string pilet = "";
+
+            if (vanus < 6)
+            {
+                pilet = "tasuta";
+                Console.WriteLine("Pilet Juku jaoks on tasuta.");
+                return;
+            }
+            else if (vanus <= 14)
+            {
+                pilet = "lastepilet";
+            }
+            else if (vanus <= 65)
+            {
+                pilet = "täispilet";
+            }
+            else
+            {
+                pilet = "sooduspilet";
+            }
+            Console.WriteLine($"Jukule on vaja: {pilet}.");
+        }
+
+        public static void Pinginaabrid()
+        {
+            Console.Write("Sisesta 1. õpilase nimi: ");
+            string n1 = Console.ReadLine();
+            Console.Write("Sisesta 2. õpilase nimi: ");
+            string n2 = Console.ReadLine();
+
+            Console.WriteLine($"{n1} ja {n2} on täna pinginaabrid.");
+        }
+
+        public static void TubaPindalaJaRemont()
+        {
+            double a = 0;
+            double b = 0;
+
+            while (true)
+            {
+                Console.Write("Sisesta sein 1 pikkus (meetrites): ");
+                try
+                {
+                    a = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sisesta palun korrektne arv (nt 3.5).");
+                }
+
+            }
+
+            while (true)
+            {
+                Console.Write("Sisesta sein 2 pikkus (meetrites): ");
+                try
+                {
+                    b = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sisesta palun korrektne arv (nt 3.5).");
+                }
+            }
+
+            if (a <= 0 || b <= 0)
+            {
+                Console.WriteLine("Pikkused peavad olema positiivsed.");
+                return;
+            }
+
+            double pindala = a * b;
+            Console.WriteLine($"Põranda pindala: {Math.Round(pindala, 2)} m2");
+
+            bool remont = false;
+            while (true)
+            {
+                Console.Write("Kas soovid remonti teha? (jah/ei): ");
+                string vastus = Console.ReadLine().ToLower();
+
+                if (vastus == "jah")
+                {
+                    remont = true;
+                    break;
+                }
+                if (vastus == "ei")
+                {
+                    remont = false;
+                    break;
+                }
+
+                Console.WriteLine("Palun vasta jah või ei.");
+            }
+
+            if (!remont)
+            {
+                Console.WriteLine("Remonti ei tehta.");
+                return;
+            }
+
+            double hind = 0;
+            while (true)
+            {
+                Console.Write("Kui palju maksab 1 m2 (eurot): ");
+                try
+                {
+                    hind = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sisesta palun korrektne arv (nt 12.5).");
+                }
+            }
+
+            if (hind < 0)
+            {
+                Console.WriteLine("Hind ei saa olla negatiivne.");
+                return;
+            }
+
+            double kokku = pindala * hind;
+            Console.WriteLine($"Põranda vahetamise hind: {Math.Round(kokku, 2)} eurot");
+        }
+
+        public static void Alghind30Soodustus()
+        {
+            double soodushind = 0;
+            while (true)
+            {
+                Console.Write("Sisesta soodushind (€): ");
+                try
+                {
+                    soodushind = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sisesta palun korrektne arv (nt 12.5).");
+                }
+            }
+
+            if (soodushind < 0)
+            {
+                Console.WriteLine("Hind ei saa olla negatiivne.");
+                return;
+            }
+
+            double alghind = soodushind / 0.7;
+            Console.WriteLine($"Alghind oli: {Math.Round(alghind, 2)} €");
+        }
+
+        public static void Temperatuur()
+        {
+            double t = 0;
+            while (true)
+            {
+                Console.Write("Sisesta temperatuur (°C): ");
+                try
+                {
+                    t = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sisesta palun korrektne arv (nt 13).");
+                }
+            }
+
+            if (t > 17.99)
+                Console.WriteLine("Temperatuur on (või üle) 18°C (soovitav toasoojus talvel).");
+            else
+                Console.WriteLine("Temperatuur on alla 18°C.");
+        }
+        public static void PikkusKategooria()
+        {
+            int p = 0;
+            while (true)
+            {
+                Console.Write("Sisesta pikkus (cm): ");
+                try
+                {
+                    p = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sisesta palun korrektne täisarv");
+                }
+            }
+
+            if (p < 160)
+            {
+                Console.WriteLine("Sa oled lühike.");
+            }
+            else if (p <= 185)
+            {
+                Console.WriteLine("Sa oled keskmine.");
+            }
+            else
+            {
+                Console.WriteLine("Sa oled pikk.");
+            }
+        }
+        public static void PikkusJaSugu()
+        {
+            Console.Write("Sisesta sugu (m/n): ");
+            string sugu = Console.ReadLine().ToLower();
+
+            int p = 0;
+            while (true)
+            {
+                Console.Write("Sisesta pikkus (cm): ");
+                try
+                {
+                    p = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sisesta palun korrektne täisarv");
+                }
+            }
+
+            if (sugu == "m")
+            {
+                if (p < 170)
+                {
+                    Console.WriteLine("Meeste jaoks on sa lühike");
+                }
+                else if (p <= 190)
+                {
+                    Console.WriteLine("Meeste jaoks on sa keskmine");
+                }
+                else
+                {
+                    Console.WriteLine("Meeste jaoks on sa pikk");
+                }
+            }
+            else if (sugu == "n")
+            {
+                if (p < 160)
+                {
+                    Console.WriteLine("Naiste jaoks on sa lühike");
+                }
+                else if (p <= 175)
+                {
+                    Console.WriteLine("Naiste jaoks on sa keskmine");
+                }
+                else
+                {
+                    Console.WriteLine("Naiste jaoks on sa pikk");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tundmatu sugu. Kasuta m või n.");
+            }
+        }
+
+        public static void PoodOstud()
+        {
+            string[] kysimused = {
+                "piima",
+                "saia",
+                "leiba"
+            };
+            string[] nimed = {
+                "piim",
+                "sai",
+                "leib"
+            };
+            double[] hinnad = {
+                1.20,
+                0.70,
+                0.50
+            };
+
+            double summa = 0;
+            List<string> ostud = new List<string>();
+
+            for (int i = 0; i < kysimused.Length; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Kas soovid osta {kysimused[i]} ({hinnad[i]} eur)? (jah/ei): ");
+                    string s = Console.ReadLine().ToLower();
+
+                    if (s == "jah")
+                    {
+                        summa += hinnad[i];
+                        ostud.Add($"{nimed[i]} ({hinnad[i]} eur)");
+                        break;
+                    }
+                    else if (s == "ei")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Palun vasta jah või ei.");
+                    }
+                }
+            }
+
+            if (ostud.Count == 0)
+            {
+                Console.WriteLine("Midagi ei ostetud.");
+            }
+            else
+            {
+                Console.WriteLine("Ostetud: " + string.Join(", ", ostud));
+                Console.WriteLine("Kokku: " + Math.Round(summa, 2) + " eur");
+            }
+        }
+
+    }
+}
