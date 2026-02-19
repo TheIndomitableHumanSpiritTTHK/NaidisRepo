9b7714c2bebc879ca93f727c196fcae627949b72
Artyom <234121450+TheIndomitableHumanSpiritTTHK@users.noreply.github.com>
2026-02-19 17:41:14 +0200

    Tehtud kommentaare moodle'i ülessanne järgi et kood oleks paremini loetav

--- NaidisRepo/Kolmas_osa_funktsioonid.cs
+++ NaidisRepo/Kolmas_osa_funktsioonid.cs
@@ -6,21 +6,26 @@ namespace NaidisRepo
 {
     public static class Kolmas_osa_funktsioonid
     {
+        // 2. Viie arvu analüüs
         public static void ViieArvuAnaluus()
         {
             Console.Clear();
             Console.WriteLine("2. Viie arvu analüüs\n");
 
+            // võtab sisendiks massiivi (kasutaja sisestab Tekstist_arvud() meetodi abil)
             double[] arvud = Tekstist_arvud();
 
+            // tagastab Tuple<double, double, double> – summa, keskmine, korrutis
             var tuple = AnalüüsiArve(arvud);
 
+            // väljastab tulemuse hästi vormindatult
             Console.WriteLine("\nTulemused:");
             Console.WriteLine("Summa: " + tuple.Item1);
             Console.WriteLine("Keskmine: " + tuple.Item2);
             Console.WriteLine("Korrutis: " + tuple.Item3);
         }
 
+        // (kasutaja sisestab Tekstist_arvud() meetodi abil)
         public static double[] Tekstist_arvud()
         {
             Console.Write("Sisesta 5 arvu (tühikuga): ");
@@ -53,11 +58,13 @@ namespace NaidisRepo
             return new Tuple<double, double, double>(summa, keskmine, korrutis);
         }
 
+        // 3. Nimed ja vanused
         public static void NimedJaVanused()
         {
             Console.Clear();
             Console.WriteLine("3. Nimed ja vanused\n");
 
+            // Kasutaja sisestab 5 inimest (nimi ja vanus eraldi).
             List<Inimene> inimesed = new List<Inimene>();
 
             for (int i = 0; i < 5; i++)
@@ -84,6 +91,7 @@ namespace NaidisRepo
             Console.WriteLine("Noorim: " + t.Item4.Nimi + " (" + t.Item4.Vanus + ")");
         }
 
+        // 3. Nimed ja vanused: Meetod Statistika(List<Inimene> inimesed)
         public static Tuple<int, double, Inimene, Inimene> Statistika(List<Inimene> inimesed)
         {
             int summa = 0;
@@ -91,6 +99,8 @@ namespace NaidisRepo
             Inimene vanim = inimesed[0];
             Inimene noorim = inimesed[0];
 
+            // arvutab kogu vanuse summa ja keskmise
+            // leiab vanima ja noorima inimese
             for (int i = 0; i < inimesed.Count; i++)
             {
                 summa = summa + inimesed[i].Vanus;
@@ -108,20 +118,23 @@ namespace NaidisRepo
 
             double keskmine = (double)summa / inimesed.Count;
 
+            // tagastab need väärtused Tuple<int, double, Inimene, Inimene>
             return new Tuple<int, double, Inimene, Inimene>(summa, keskmine, vanim, noorim);
         }
 
+        // 4. "Osta elevant ära!"
         public static void OstaElevantAra()
         {
             Console.Clear();
             Console.WriteLine("4. Osta elevant ära!\n");
 
             string marksõna = "okei ma ostan";
-
             string fraas = $"Osta elevant ära! (Sisesta '{marksõna}' ostmiseks)";
 
+            // Tee korduv sisendimeetod KuniMärksõnani(string märksõna, string fraas)
             List<string> sisestused = KuniMarksonani(marksõna, fraas);
 
+            // kõik sisestused salvestatakse ja trükitakse lõpuks välja
             Console.WriteLine("\nKogu dialoog:");
             for (int i = 0; i < sisestused.Count; i++)
             {
@@ -129,18 +142,22 @@ namespace NaidisRepo
             }
         }
 
+        // 4. "Osta elevant ära!": KuniMärksõnani(string märksõna, string fraas)
         public static List<string> KuniMarksonani(string marksona, string fraas)
         {
             List<string> lst = new List<string>();
 
             while (true)
             {
+                // iga kord enne kirjutab uuesti fraas
                 Console.WriteLine(fraas);
                 Console.Write("Sinu vastus: ");
                 string vastus = Console.ReadLine();
 
+                // kõik sisestused salvestatakse
                 lst.Add(vastus);
 
+                // küsib kasutajalt sisendit seni, kuni see täpselt võrdub märksõnaga
                 if (vastus == marksona)
                 {
                     Console.WriteLine("Tubli! Ostad elevandi ära!");
@@ -149,24 +166,28 @@ namespace NaidisRepo
 
                 string meie_vastus = $"Seda räägivad kõik, et {vastus}, aga osta elevant ära!";
                 lst.Add(meie_vastus);
-                Console.WriteLine(meie_vastus+"\n");
+                Console.WriteLine(meie_vastus + "\n");
             }
 
             return lst;
         }
 
-
+        // 5. Arvamise mäng
         public static void ArvamiseMang()
         {
             Console.Clear();
             Console.WriteLine("5. Arvamise mäng\n");
 
+            // Arvuti valib juhuslikult arvu [1–100].
             Random rnd = new Random();
 
             while (true)
             {
+                // Kasutajal on 5 katset.
+                // mäng lõppeb õigel arvamisel või viienda katse järel.
                 ArvaArv(rnd);
 
+                // Pärast mängu küsime, kas kasutaja soovib uuesti mängida.
                 Console.Write("\nKas soovid uuesti mängida? (j/e): ");
                 string vastus = Console.ReadLine();
 
@@ -178,19 +199,22 @@ namespace NaidisRepo
                 Console.Clear();
             }
         }
-
         public static void ArvaArv(Random rnd)
         {
+            // Arvuti valib juhuslikult arvu [1–100].
             int juhuArv = rnd.Next(1, 101);
 
+            // Kasutajal on 5 katset.
             for (int katse = 1; katse <= 5; katse++)
             {
                 Console.Write("Katse " + katse + "/5. Sisesta arv (1-100): ");
                 int arv = int.Parse(Console.ReadLine());
 
+                // igal katsel ütleb, kas "liiga suur", "liiga väike" või "õige"
                 if (arv == juhuArv)
                 {
                     Console.WriteLine($"Õige! Arv oli {juhuArv}.");
+                    // mäng lõppeb õigel arvamisel
                     return;
                 }
                 else if (arv > juhuArv)
@@ -203,10 +227,11 @@ namespace NaidisRepo
                 }
             }
 
+            // mäng lõppeb ... viienda katse järel
             Console.WriteLine("Katseid sai otsa! = ( Õige arv oli " + juhuArv);
         }
 
-
+        // 7. Korrutustabel
         public static void Korrutustabel()
         {
             Console.Clear();
@@ -218,6 +243,7 @@ namespace NaidisRepo
             Console.Write("Sisesta veergude arv: ");
             int veerud = int.Parse(Console.ReadLine());
 
+            // Kirjuta meetod GenereeriKorrutustabel(int ridadeArv, int veergudeArv)
             GenereeriKorrutustabel(read, veerud);
         }
 
@@ -237,16 +263,20 @@ namespace NaidisRepo
             return tabel;
         }
 
+        // 8. Õpilastega mängimine
         public static void OpilastegaMangimine()
         {
             Console.Clear();
             Console.WriteLine("8. Õpilastega mängimine\n");
 
+            // Antud on 10 õpilase nimede massiiv.
             string[] nimed = ["Artjom", "Mari", "Andres", "Anna", "Alo", "Peeter", "Ann", "Juku", "Siim", "Deniss"];
 
+            // Asendab kolmanda ja kuuenda õpilase nime (indeksite põhjal) uue nimega "Kati" ja "Mati".
             nimed[2] = "Kati";
             nimed[5] = "Mati";
 
+            // Kasutab while tsüklit, et tervitada ainult õpilasi, kelle nimi algab tähega "A".
             int i = 0;
             while (i < nimed.Length)
             {
@@ -257,19 +287,19 @@ namespace NaidisRepo
                 i = i + 1;
             }
 
-            //Kasutab for tsüklit, et väljastada kõik nimed ja nende indeksid
+            // Kasutab for tsüklit, et väljastada kõik nimed ja nende indeksid.
             for (int j = 0; j < nimed.Length; j++)
             {
                 Console.WriteLine(j + " - " + nimed[j]);
             }
 
-            //Kasutab foreach tsüklit, et väljastada kõik nimed väikeste tähtedena.
+            // Kasutab foreach tsüklit, et väljastada kõik nimed väikeste tähtedena.
             foreach (var nimi in nimed)
             {
                 Console.WriteLine(nimi.ToLower());
             }
 
-            //Kasutab do-while tsüklit, et tervitada õpilasi seni, kuni kohtab nime "Mati".
+            // Kasutab do-while tsüklit, et tervitada õpilasi seni, kuni kohtab nime "Mati".
             int i2 = 0;
             do
             {
@@ -286,25 +316,30 @@ namespace NaidisRepo
             } while (i2 < nimed.Length);
         }
 
+        // 9 – Arvude ruudud
         public static void ArvudeRuudud()
         {
             Console.Clear();
             Console.WriteLine("9. Arvude ruudud\n");
 
+            // Antud on massiiv täisarvudega: int[] arvud = { 2, 4, 6, 8, 10, 12 };
             int[] arvud = { 2, 4, 6, 8, 10, 12 };
 
+            // Kasutab for tsüklit, et väljastada iga arvu ruut.
             Console.WriteLine("ruudud (for)");
             for (int i = 0; i < arvud.Length; i++)
             {
                 Console.WriteLine(arvud[i] + " -> " + (arvud[i] * arvud[i]));
             }
 
+            // Kasutab foreach tsüklit, et väljastada iga arvu kahekordne väärtus.
             Console.WriteLine("\n kahekordne arvu väärtus (foreach)");
             foreach (int a in arvud)
             {
                 Console.WriteLine(a + " -> " + (a * 2));
             }
 
+            // Kasutab while tsüklit, et loendada kui palju on arvude seas arve, mis jaguvad 3-ga.
             Console.WriteLine("\nkui palju jagub 3-ga (while)");
             int nr = 0;
             int count = 0;
@@ -319,17 +354,20 @@ namespace NaidisRepo
             Console.WriteLine($"{count} numbrid jaguvad 3-ga");
         }
 
+        // 10 – Positiivsed ja negatiivsed
         public static void PositNegNull()
         {
             Console.Clear();
             Console.WriteLine("10. Positiivsed ja negatiivsed\n");
 
+            // Antud on massiiv 12 arvuga (sealhulgas negatiivsed, positiivsed ja nullid):
             int[] arvud = { 5, -3, 0, 8, -1, 4, -7, 2, 0, -5, 6, 9 };
 
             int pos = 0;
             int neg = 0;
             int zero = 0;
 
+            // Loe kui palju on: positiivseid arve, negatiivseid arve ja nulle
             foreach (int a in arvud)
             {
                 if (a > 0)
@@ -340,8 +378,9 @@ namespace NaidisRepo
                 {
                     neg += 1;
                 }
-                else { 
-                    zero += 1; 
+                else
+                {
+                    zero += 1;
                 }
             }
 
@@ -350,11 +389,13 @@ namespace NaidisRepo
             Console.WriteLine($"Nulle: {zero}");
         }
 
+        // 11 – Keskmisest suuremad
         public static void KeskmisestSuuremad()
         {
             Console.Clear();
             Console.WriteLine("11. Keskmisest suuremad\n");
 
+            // Genereeri 15 juhuslikku arvu vahemikus 1–100 ja salvesta need int[] massiivi.
             Random rnd = new Random();
             int[] arvud = new int[15];
 
@@ -366,6 +407,7 @@ namespace NaidisRepo
                 summa = summa + arvud[i];
             }
 
+            // Leia keskmine väärtus (summa / arvude arv).
             double keskmine = (double)summa / arvud.Length;
 
             Console.WriteLine("Arvud:");
@@ -377,6 +419,7 @@ namespace NaidisRepo
             Console.WriteLine("\n\nKeskmine: " + keskmine);
             Console.WriteLine("Suuremad kui keskmine:");
 
+            // Väljasta ainult need arvud, mis on suuremad kui keskmine.
             for (int i = 0; i < arvud.Length; i++)
             {
                 if (arvud[i] > keskmine)
@@ -385,6 +428,7 @@ namespace NaidisRepo
                 }
             }
 
+            // Kasuta do-while tsüklit arvude väljastamiseks seni, kuni kohtad arvu, mis on väiksem kui 10.
             Console.WriteLine("\n\nVäljastame numbreid kuni kohtame arvu < 10 do-whileiga");
             int idx = 0;
             do
@@ -402,21 +446,26 @@ namespace NaidisRepo
             Console.WriteLine();
         }
 
+        // 12 – Kõige suurema arvu otsing
         public static void SuurimArvJaIndeks()
         {
             Console.Clear();
             Console.WriteLine("12. Kõige suurema arvu otsing\n");
 
+            // Antud: int[] numbrid = { 12, 56, 78, 2, 90, 43, 88, 67 };
             int[] numbrid = { 12, 56, 78, 2, 90, 43, 88, 67 };
 
             int max = numbrid[0];
             int maxIndex = 0;
 
+            // Kasutab for tsüklit.
             for (int i = 1; i < numbrid.Length; i++)
             {
+                // Leiab suurima arvu massiivis (ilma Max() kasutamata).
                 if (numbrid[i] > max)
                 {
                     max = numbrid[i];
+                    // Kuvab selle arvu indeksi, mitte ainult väärtuse.
                     maxIndex = i;
                 }
             }
@@ -425,15 +474,17 @@ namespace NaidisRepo
             Console.WriteLine("Indeks: " + maxIndex);
         }
 
+        // 13 – Paari- ja paaritud loendused
         public static void PaarJaPaarituStat()
         {
             Console.Clear();
             Console.WriteLine("13. Paari- ja paaritud loendused\n");
 
+            // Genereeri List<int> 20 juhuslikust täisarvust.
             Random rnd = new Random();
             List<int> arvud = new List<int>();
 
-            // Genereerime listisse 20 juhuslikud täisarvud
+            // Kasuta vaheldumisi for, foreach ja while
             for (int i = 0; i < 20; i++)
             {
                 arvud.Add(rnd.Next(1, 101));
@@ -446,7 +497,7 @@ namespace NaidisRepo
             }
             Console.WriteLine();
 
-            // foreach: even sum
+            // Paarisarvude kogusumma (foreach)
             int paarSumma = 0;
             foreach (int a in arvud)
             {
@@ -456,7 +507,7 @@ namespace NaidisRepo
                 }
             }
 
-            // for: odd avg
+            // Paaritute arvude keskmine (for)
             int paarituSumma = 0;
             int paarituCount = 0;
 
@@ -475,7 +526,7 @@ namespace NaidisRepo
                 paarituKeskmine = (double)paarituSumma / paarituCount;
             }
 
-            // mitu arvu on suuremad kui 50
+            // Loendada, mitu arvu on suuremad kui 50 (while)
             int idx = 0;
             int suurem50 = 0;
             while (idx < arvud.Count)
--- NaidisRepo/Teine_osa_funktsioonid.cs
+++ NaidisRepo/Teine_osa_funktsioonid.cs
@@ -2,12 +2,19 @@
 {
     public class Teine_osa_funktsioonid
     {
+        // *Kui eesnimi on Juku siis lähme Jukuga kinno. Lisa valiku, kus Juku vanuse alusel otsustate mis pilet talle vaja osta.
+        // <6 aastad  - tasuta
+        // 6-14 - lastepilet
+        // 15-65 - täispilet
+        // >65 - sooduspilet
+        // <0 ja >100 viga andmetega
         public static void JukuKino()
         {
             Console.Write("Sisesta eesnimi: ");
             string eesnimi = Console.ReadLine();
             int vanus = 0;
 
+            // *Kui eesnimi on Juku siis lähme Jukuga kinno.
             if (eesnimi.ToLower() != "juku")
             {
                 Console.WriteLine("See ülesanne on Juku jaoks — sinu nimi pole Juku.");
@@ -29,6 +36,7 @@
                 }
             }
 
+            // <0 ja >100 viga andmetega
             if (vanus < 0 || vanus > 100)
             {
                 Console.WriteLine("Vigased andmed. Vanus peab olema suurem kui 0 ja väiksem kui 100.");
@@ -37,20 +45,24 @@
 
             string pilet = "";
 
+            // <6 aastad  - tasuta
             if (vanus < 6)
             {
                 pilet = "tasuta";
                 Console.WriteLine("Pilet Juku jaoks on tasuta.");
                 return;
             }
+            // 6-14 - lastepilet
             else if (vanus <= 14)
             {
                 pilet = "lastepilet";
             }
+            // 15-65 - täispilet
             else if (vanus <= 65)
             {
                 pilet = "täispilet";
             }
+            // >65 - sooduspilet
             else
             {
                 pilet = "sooduspilet";
@@ -58,6 +70,7 @@
             Console.WriteLine($"Jukule on vaja: {pilet}.");
         }
 
+        // Küsi kahe inimese nimed ning teata, et nad on täna pinginaabrid
         public static void Pinginaabrid()
         {
             Console.Write("Sisesta 1. õpilase nimi: ");
@@ -68,11 +81,14 @@
             Console.WriteLine($"{n1} ja {n2} on täna pinginaabrid.");
         }
 
+        // Küsi ristkülikukujulise toa seinte pikkused ning arvuta põranda pindala.
+        // Küsi kasutajalt remondi tegemise soov, kui ta on positiivne, siis küsi kui palju maksab ruutmeeter ja leia põranda vahetamise hind
         public static void TubaPindalaJaRemont()
         {
             double a = 0;
             double b = 0;
 
+            // Küsi ristkülikukujulise toa seinte pikkused
             while (true)
             {
                 Console.Write("Sisesta sein 1 pikkus (meetrites): ");
@@ -88,6 +104,7 @@
 
             }
 
+            // Küsi ristkülikukujulise toa seinte pikkused
             while (true)
             {
                 Console.Write("Sisesta sein 2 pikkus (meetrites): ");
@@ -108,12 +125,14 @@
                 return;
             }
 
+            // * ... arvuta põranda pindala
             double pindala = a * b;
             Console.WriteLine($"Põranda pindala: {Math.Round(pindala, 2)} m2");
 
             bool remont = false;
             while (true)
             {
+                // Küsi kasutajalt remondi tegemise soov
                 Console.Write("Kas soovid remonti teha? (jah/ei): ");
                 string vastus = Console.ReadLine().ToLower();
 
@@ -140,6 +159,7 @@
             double hind = 0;
             while (true)
             {
+                // * ... kui ta on positiivne, siis küsi kui palju maksab ruutmeeter
                 Console.Write("Kui palju maksab 1 m2 (eurot): ");
                 try
                 {
@@ -158,10 +178,12 @@
                 return;
             }
 
+            // * ... leia põranda vahetamise hind
             double kokku = pindala * hind;
             Console.WriteLine($"Põranda vahetamise hind: {Math.Round(kokku, 2)} eurot");
         }
 
+        // Leia 30% hinnasoodustusega hinna põhjal alghind
         public static void Alghind30Soodustus()
         {
             double soodushind = 0;
@@ -185,15 +207,18 @@
                 return;
             }
 
+            // Leia 30% hinnasoodustusega hinna põhjal alghind
             double alghind = soodushind / 0.7;
             Console.WriteLine($"Alghind oli: {Math.Round(alghind, 2)} €");
         }
 
+        // Küsi temperatuur ning teata, kas see on üle kaheksateistkümne kraadi (soovitav toasoojus talvel).
         public static void Temperatuur()
         {
             double t = 0;
             while (true)
             {
+                // Küsi temperatuur
                 Console.Write("Sisesta temperatuur (°C): ");
                 try
                 {
@@ -206,16 +231,20 @@
                 }
             }
 
+            // * ... teata, kas see on üle kaheksateistkümne kraadi (soovitav toasoojus talvel).
             if (t > 17.99)
                 Console.WriteLine("Temperatuur on (või üle) 18°C (soovitav toasoojus talvel).");
             else
                 Console.WriteLine("Temperatuur on alla 18°C.");
         }
+
+        // Küsi inimese pikkus ning teata, kas ta on lühike, keskmine või pikk
         public static void PikkusKategooria()
         {
             int p = 0;
             while (true)
             {
+                // * Küsi inimese pikkus
                 Console.Write("Sisesta pikkus (cm): ");
                 try
                 {
@@ -228,6 +257,7 @@
                 }
             }
 
+            // * ... teata, kas ta on lühike, keskmine või pikk
             if (p < 160)
             {
                 Console.WriteLine("Sa oled lühike.");
@@ -241,14 +271,18 @@
                 Console.WriteLine("Sa oled pikk.");
             }
         }
+
+        //Küsi inimeselt pikkus ja sugu ning teata, kas ta on lühike, keskmine või pikk.
         public static void PikkusJaSugu()
         {
+            //Küsi inimeselt ... sugu
             Console.Write("Sisesta sugu (m/n): ");
             string sugu = Console.ReadLine().ToLower();
 
             int p = 0;
             while (true)
             {
+                // Küsi inimeselt ... pikkus
                 Console.Write("Sisesta pikkus (cm): ");
                 try
                 {
@@ -261,6 +295,7 @@
                 }
             }
 
+            // * ... teata, kas ta on lühike, keskmine või pikk.
             if (sugu == "m")
             {
                 if (p < 170)
@@ -297,6 +332,7 @@
             }
         }
 
+        // Küsi inimeselt poes eraldi kas ta soovib osta piima, saia, leiba. Löö hinnad kokku ning teata, mis kõik ostetud kraam maksma läheb.
         public static void PoodOstud()
         {
             string[] kysimused = {
@@ -318,6 +354,7 @@
             double summa = 0;
             List<string> ostud = new List<string>();
 
+            // Küsi inimeselt poes eraldi kas ta soovib osta piima, saia, leiba.
             for (int i = 0; i < kysimused.Length; i++)
             {
                 while (true)
@@ -327,6 +364,7 @@
 
                     if (s == "jah")
                     {
+                        // Arvame hinnad kokku
                         summa += hinnad[i];
                         ostud.Add($"{nimed[i]} ({hinnad[i]} eur)");
                         break;
@@ -342,6 +380,7 @@
                 }
             }
 
+            // ... teata, mis kõik ostetud kraam maksma läheb.
             if (ostud.Count == 0)
             {
                 Console.WriteLine("Midagi ei ostetud.");
